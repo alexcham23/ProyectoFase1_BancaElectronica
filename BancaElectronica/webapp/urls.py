bff9c37b181b16726ffd72d4e043c863916a51e0
alexcham23 <tec-james.armira@outlook.com>
2020-12-13 00:08:30 -0600

    update

--- BancaElectronica/webapp/urls.py
+++ BancaElectronica/webapp/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import  path
 from webapp import views
 urlpatterns=[
-    path('',views.BancaPrincipal)
+    path('',views.BancaPrincipal),
+    path('EstadoCuenta/',views.EstadoCuenta)
 ]
--- BancaElectronica/webapp/views.py
+++ BancaElectronica/webapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def BancaPrincipal(request):
     return render(request,'Cuenta.html')
+def EstadoCuenta(request):
+    return render(request,'Estado.html')
